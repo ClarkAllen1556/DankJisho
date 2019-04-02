671f5a8854c6d20dc3f8ac2573a49503f457c1cf
Allen <allenclark@u.boisestate.edu>
2019-04-02 13:08:52 -0600

    Black user page and moving forum button.

--- forum.php
+++ forum.php
@@ -51,19 +51,14 @@ session_start();
 				. "</td><td> {$post['Posted']} </td></tr>";
 			}
 			echo "</table>";
-			?>
-		</div>
-		<div>
-			<?php
+
 			if (isset($_SESSION['logged_in'])) {
 			?>
 				<form method='post' action='./new_forum_post.php'>
 					<button type='button id='NewPost' value='NewPost' name='newpost' href='./new_forum_post.php'> New Post </button>
 				</form>
 			<?php
-			} /*else {
-				echo "<input id='NewPost' type='submit' value='Login' href='login.php'>";
-			}*/
+			}
 			?>
 		</div>
 	</div>
---
+++ userpage.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<?php
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+?>
+
+<html>
+<head>
+	<link rel="stylesheet" href="styles/global.css">
+</head>
+<header>
+	<h1>
+		User page
+	</h1>
+</header>
+<body>
+	<p>
+		Page coming soon...
+	</p>
+	<p>
+		<a class='Link' href='./index.php'> < Back to DankJisho </a>
+	</p>
+</body>
+</html>
