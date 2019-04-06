eb172786bc465fa8b9684ab03bbae51c1b11b650
Allen <allenclark@u.boisestate.edu>
2019-04-06 14:04:08 -0600

    About page link modifications

--- about.php
+++ about.php
@@ -37,39 +37,38 @@ session_start();
 
 <body>
 	<h2> About Dank Jisho </h2>
-	<div class="Column">
+	<div class='Column'>
 		<p>
-			This website is made by <span title="I really like curry">Allen Clark</span> of Boise State University. More content is on the way...
+			This website is made by <span title='I really like curry'>Allen Clark</span> of Boise State University. More content is on the way...
 		</p>
 		<p>
 			Links to my things:
 		</p>
 		<p>
-			<a href='https://github.com/ClarkAllen1556/DankJisho'>Dank Jisho: GitHub</a>
-			<a href='https://github.com/ClarkAllen1556'>
-				<img src="https://cdn4.iconfinder.com/data/icons/iconsimple-logotypes/512/github-512.png" alt="GitHub" height="30" width="30" />
+			<a class='NamedLink' href='https://github.com/ClarkAllen1556/DankJisho' target='_blank'>
+				<img src='https://cdn4.iconfinder.com/data/icons/iconsimple-logotypes/512/github-512.png' alt='GitHub' height='30' width='30' />
 			</a>
-			<a rel="me" href='https://toot.cafe/@The_A_C'>
-				<img src="https://toot.cafe/packs/logo-fe5141d38a25f50068b4c69b77ca1ec8.svg" alt="Mastodon" height="30" width="30" />
+			<a class='NamedLink' rel='me' href='https://toot.cafe/@The_A_C' target='_blank'>
+				<img src='https://toot.cafe/packs/logo-fe5141d38a25f50068b4c69b77ca1ec8.svg' alt='Mastodon' height='30' width='30' />
 			</a>
-			<a href="https://dev.to/allen1556">
-				<img src="https://d2fltix0v2e0sb.cloudfront.net/dev-badge.svg" alt="Allen's DEV Profile" height="30" width="30" />
+			<a class='NamedLink' href='https://dev.to/allen1556' target='_blank'>
+				<img src='https://d2fltix0v2e0sb.cloudfront.net/dev-badge.svg' alt='Allen's DEV Profile' height='30' width='30' />
 			</a>
 		</p>
 	</div>
 </body>
-<div id="Footer">
+<div id='Footer'>
 	<span>
-		<a class="Link" href="about.php"> About |</a>
+		<a class='Link' href='about.php'> About |</a>
 	</span>
 	<span>
-		<a class="Link" href="forum.php"> Forums |</a>
+		<a class='Link' href='forum.php'> Forums |</a>
 	</span>
 	<span>
-		<a class="Link" href="index.php"> Home |</a>
+		<a class='Link' href='index.php'> Home |</a>
 	</span>
 	<span>
-		<a class="Link" href="recent.php"> Recent </a>
+		<a class='Link' href='recent.php'> Recent </a>
 	</span>
 </div>
 
